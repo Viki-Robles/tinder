6d7ef0bce5776ea673713e5b5a2e4dd4b351cbdf
Vicky Vasilopoulou <vickyvasilopoulou@vickys-macbook.lan>
2020-07-07 10:28:56 +0100

    Add cards

---
+++ src/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css";
+
+export default function Card({ name, title, picture, age }) {
+    return (
+        <div className="card">
+            <div className="card-container">
+                <img src={picture} />
+                <div className="card-name">{name}, {age}</div>
+                <div className="card-title">{title}</div>
+            </div>
+        </div>
+    )
+}
---
+++ src/Cards/cardsData.js
@@ -0,0 +1,13 @@
+import builder2 from "./images/builder2.png";
+
+
+export default function cardsData(){
+    return [
+    { id: 1, name: "George", age: 25, picture: builder2 },
+    { id: 2,name: "Nick", age: 35, picture: "" },
+    { id: 3,name: "John", age: 26, picture: "" },
+    { id: 4,name: "Michael", age: 40, picture: "" },
+    { id: 5,name: "Bill", age: 28, picture: "" },
+    { id: 6,name: "Panos", age: 33, picture: "" }
+    ]
+}
